d2aa8f2122431593bd2297961b876ed21d28d2aa
KalliMiika <44061875+KalliMiika@users.noreply.github.com>
2020-11-19 19:13:49 +0200

    Korjaus
    Livahti vähän testailukoodia pushaukseen D:

--- src/main/java/miniprojekti/Main.java
+++ src/main/java/miniprojekti/Main.java
@@ -11,24 +11,5 @@ import miniprojekti.domain.ReadingTip;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test");
-        Database db = new Database("jdbc:sqlite:readingtips.db");
-        ReadingTipDao readingtipdao = new ReadingTipDao(db);
-        
-        ReadingTip tip = new ReadingTip(0, "Ensio Esimerkki", "Testi Titteli", "Kuvaava Kuvaus", "https://ohjelmistotuotanto-hy.github.io/speksi/", "Esimerkki", "Tämä on esimerkki");
-        try {
-            readingtipdao.save(tip);
-            for (ReadingTip rt : readingtipdao.findAll()) {
-                System.out.println(
-                        "Author: \t" + rt.getAuthor() + "\n"
-                        + "Title: \t" + rt.getTitle() + "\n"
-                        + "Description: \t" + rt.getDescription() + "\n"
-                        + "Url: \t" + rt.getUrl() + "\n"
-                        + "Type: \t" + rt.getType() + "\n"
-                        + "Note: \t" + rt.getNote() + "\n"
-                );
-            }
-        } catch(SQLException sqle) {
-            sqle.printStackTrace();
-        }
     }
 }
