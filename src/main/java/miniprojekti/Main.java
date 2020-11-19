56279b9ecb23765daee5cce0b7979a1b3ce43fd8
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-11-19 21:09:19 +0200

    Spark helloworld herokua varten

--- src/main/java/miniprojekti/Main.java
+++ src/main/java/miniprojekti/Main.java
@@ -2,14 +2,27 @@ package miniprojekti;
 
 import java.sql.SQLException;
 import java.util.List;
-import miniprojekti.data_access.ReadingTipDao;
-import miniprojekti.database.Database;
-import miniprojekti.domain.ReadingTip;
-
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
 
 // Main tulee toimimaan Controllerina
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test");
+        
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        
+        if (process.environment().get("PORT") != null) {
+        	port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+        	port = 4567;
+        }
+        
+        Spark.port(port);
+        
+        Spark.get("/helloworld", (req, res) -> "Hello World");
     }
 }
