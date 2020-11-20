40080b5407cea0a38afc7a1989cb0b486512b6c7
ktatu <kaikkonentatu@gmail.com>
2020-11-20 12:50:23 +0200

    controller ja template etusivulle

--- src/main/java/miniprojekti/Main.java
+++ src/main/java/miniprojekti/Main.java
@@ -1,11 +1,16 @@
 package miniprojekti;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.Spark;
+import static spark.Spark.get;
+import spark.template.velocity.VelocityTemplateEngine;
 
 // Main tulee toimimaan Controllerina
 public class Main {
@@ -24,5 +29,15 @@ public class Main {
         Spark.port(port);
         
         Spark.get("/helloworld", (req, res) -> "Hello World");
+        
+        // Etusivu
+        get("/", (req, res) -> {
+            
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("test1", "test2");
+            
+            ModelAndView mod = new ModelAndView(map, "templates/index.html");
+            return mod;
+        }, new VelocityTemplateEngine());
     }
 }
