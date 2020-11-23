af56624d2ac82d48b9ba5705f2c7e8ecab3aac26
Minna Iholin <ihqminna@helsinki.fi>
2020-11-23 21:27:16 +0200

    vajaa cucumber-testi vinkin lisäyksestä

--- src/test/java/miniprojekti/Stepdefs.java
+++ src/test/java/miniprojekti/Stepdefs.java
@@ -4,43 +4,34 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
-import java.util.ArrayList;
-import java.util.List;
-import static org.junit.Assert.*;
 import miniprojekti.domain.*;
 import miniprojekti.data_access.*;
 import static org.mockito.Mockito.*;
 
 public class Stepdefs {
     Logic logic;
-    ReadingTipDao testDao;
-    List<String> inputLines;
-
+    ReadingTipDao testdao;
     
     @Before
     public void setup(){
-        testDao = mock(ReadingTipDao.class);
-        logic = new Logic(testDao);     
+        testdao = mock(ReadingTipDao.class);
+        logic = new Logic(testdao);     
     }
-
-
 
     @Given("add is selected")
     public void addIsSelected() {
-        // Write code here that turns the phrase above into concrete actions
+        // miten lomakkeen lahetys tarkistetaan?
         throw new io.cucumber.java.PendingException();
     }
-
 
     @When("new reading tip is entered")
     public void newReadingTipIsEntered() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        verify(logic).saveNewTip(anyString(), anyString(), anyString());
     }
+    
     @Then("new reading tip is added")
     public void newReadingTipIsAdded() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        verify(testdao).save(anyString(), anyString(), anyString());
     }
 }
 
