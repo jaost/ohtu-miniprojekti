d19c042740da359d5697925cb858636061924177
KalliMiika <miika.kallioinen@helsinki.fi>
2020-11-27 08:28:51 +0200

    create table lauseet lisätty

--- src/main/java/miniprojekti/database/Database.java
+++ src/main/java/miniprojekti/database/Database.java
@@ -22,12 +22,85 @@ public class Database {
                     "author VARCHAR(255), " +
                     "title VARCHAR(255), " +
                     "url VARCHAR(255))");
+                s.execute("CREATE TABLE IF NOT EXISTS tip ("
+                        + "	id INTEGER NOT NULL,"
+                        + "	otsikko VARCHAR(144),"
+                        + "	type VARCHAR(144),"
+                        + "	note VARCHAR(144),"
+                        + "	PRIMARY KEY (id)"
+                        + ")");
+                s.execute("CREATE TABLE IF NOT EXISTS book ("
+                        + "	id INTEGER NOT NULL,"
+                        + "	tip_id INTEGER NOT NULL,"
+                        + "	author VARCHAR(144),"
+                        + "	isbn VARCHAR(144),"
+                        + "	url VARCHAR(144),"
+                        + "	PRIMARY KEY (id),"
+                        + "	FOREIGN KEY(tip_id) REFERENCES tip (id)"
+                        + ")");
+                s.execute("CREATE TABLE IF NOT EXISTS video ("
+                        + "	id INTEGER NOT NULL,"
+                        + "	tip_id INTEGER NOT NULL,"
+                        + "	url VARCHAR(144),"
+                        + "	PRIMARY KEY (id),"
+                        + "	FOREIGN KEY(tip_id) REFERENCES tip (id)"
+                        + ")");
+                s.execute("CREATE TABLE IF NOT EXISTS blogpost ("
+                        + "	id INTEGER NOT NULL,"
+                        + "	tip_id INTEGER NOT NULL,"
+                        + "	url VARCHAR(144),"
+                        + "	PRIMARY KEY (id),"
+                        + "	FOREIGN KEY(tip_id) REFERENCES tip (id)"
+                        + ")");
+                s.execute("CREATE TABLE IF NOT EXISTS podcast ("
+                        + "	id INTEGER NOT NULL,"
+                        + "	tip_id INTEGER NOT NULL,"
+                        + "	author VARCHAR(144),"
+                        + "	description VARCHAR(144),"
+                        + "	url VARCHAR(144),"
+                        + "	PRIMARY KEY (id),"
+                        + "	FOREIGN KEY(tip_id) REFERENCES tip (id)"
+                        + ")");
             } else {
-                s.execute("CREATE TABLE IF NOT EXISTS readingtip " +
-                    "(id SERIAL PRIMARY KEY, " +
-                    "author VARCHAR(255), " +
-                    "title VARCHAR(255), " +
-                    "url VARCHAR(255))");
+                s.execute("CREATE TABLE IF NOT EXISTS readingtip "
+                        + "(id SERIAL PRIMARY KEY, "
+                        + "author VARCHAR(255), "
+                        + "title VARCHAR(255), "
+                        + "url VARCHAR(255))");
+                s.execute("CREATE TABLE IF NOT EXISTS tip ("
+                        + "	id SERIAL PRIMARY KEY,"
+                        + "	otsikko VARCHAR(255),"
+                        + "	type VARCHAR(255),"
+                        + "	note VARCHAR(255)"
+                        + ")");
+                s.execute("CREATE TABLE IF NOT EXISTS book ("
+                        + "	id SERIAL PRIMARY KEY,"
+                        + "	tip_id INT NOT NULL,"
+                        + "	author VARCHAR(255),"
+                        + "	isbn VARCHAR(255),"
+                        + "	url VARCHAR(255),"
+                        + "	FOREIGN KEY(tip_id) REFERENCES tip (id)"
+                        + ")");
+                s.execute("CREATE TABLE IF NOT EXISTS video ("
+                        + "	id SERIAL PRIMARY KEY,"
+                        + "	tip_id INT NOT NULL,"
+                        + "	url VARCHAR(255),"
+                        + "	FOREIGN KEY(tip_id) REFERENCES tip (id)"
+                        + ")");
+                s.execute("CREATE TABLE IF NOT EXISTS blogpost ("
+                        + "	id SERIAL PRIMARY KEY,"
+                        + "	tip_id INT NOT NULL,"
+                        + "	url VARCHAR(255),"
+                        + "	FOREIGN KEY(tip_id) REFERENCES tip (id)"
+                        + ")");
+                s.execute("CREATE TABLE IF NOT EXISTS podcast ("
+                        + "	id SERIAL PRIMARY KEY,"
+                        + "	tip_id INT NOT NULL,"
+                        + "	author VARCHAR(255),"
+                        + "	description VARCHAR(255),"
+                        + "	url VARCHAR(255),"
+                        + "	FOREIGN KEY(tip_id) REFERENCES tip (id)"
+                        + ")");
             }
 
         } catch (Exception e) {
